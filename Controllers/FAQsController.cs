11ea6b112cb1fe3bf60f33dad036a393e17142e2
Manuel Alvarez <gr200110536@elgrullo.tecmm.edu.mx>
2024-05-02 09:18:57 -0600

    Basico
    Funcionalidad Faqs

--- Controllers/FAQsController.cs
+++ Controllers/FAQsController.cs
@@ -22,5 +22,25 @@ namespace TecApi.Controllers
         {
             return _context.FAQ.ToList();
         }
+
+        [HttpGet]
+        [Route("GetFAQsId")]
+        public IEnumerable<FAQs> GetFAQsId()
+        {
+            //Solo regresa los id de los FAQs
+            return _context.FAQ.Select(a => new FAQs { IdPregunta = a.IdPregunta }).ToList();
+        }
+
+        [HttpGet]
+        [Route("GetFAQ/{id}", Name = "GetFAQ")]
+        public IActionResult GetFAQByID(int id)
+        {
+            var faq = _context.FAQ.FirstOrDefault(a => a.IdPregunta == id);
+            if (faq == null)
+            {
+                return NotFound();
+            }
+            return Ok(faq);
+        }
     }
 }
