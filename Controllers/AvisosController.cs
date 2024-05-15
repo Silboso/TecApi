b5428435c5b6ffe267407c8e8df31fd5a51db0c2
Mane087 <manealaniz95@gmail.com>
2024-05-15 17:55:56 -0600

    feat: ruta para añadir un aviso

--- Controllers/AvisosController.cs
+++ Controllers/AvisosController.cs
@@ -81,5 +81,42 @@ namespace TecApi.Controllers
                 .Where(c => c.IdAviso == id)
                 .ToList();
         }
+        [HttpPost]
+        [Route("CreateAviso")]
+        public IActionResult CreateAviso(Avisos aviso)
+        {
+            // Validar que el IdAviso no exista ya en la base de datos
+            if (_context.Aviso.Any(a => a.IdAviso == aviso.IdAviso))
+            {
+                return BadRequest("El IdAviso ya existe.");
+            }
+
+            // Validar que el IdUsuario exista en la base de datos
+            var usuarioExiste = _context.Usuario.Any(u => u.IdUsuario == aviso.IdUsuario);
+            if (!usuarioExiste)
+            {
+                return BadRequest("El IdUsuario no existe.");
+            }
+
+            // Validar que la etiqueta ya existe en la base de datos
+            var etiquetaExistente = _context.Etiqueta.SingleOrDefault(e => e.IdEtiqueta == aviso.Etiqueta.IdEtiqueta);
+            if (etiquetaExistente != null)
+            {
+                aviso.Etiqueta = etiquetaExistente;
+            }
+
+            // Validar que el usuario ya existe en la base de datos
+            var usuarioExistente = _context.Usuario.SingleOrDefault(u => u.IdUsuario == aviso.IdUsuario);
+            if (usuarioExistente != null)
+            {
+                aviso.Usuario = usuarioExistente;
+            }
+
+            _context.Aviso.Add(aviso);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetAviso", new { id = aviso.IdAviso }, aviso);
+        }
+
+
     }
 }
